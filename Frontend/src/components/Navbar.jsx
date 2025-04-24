ef38f2738db1475cbcfd1362c4efa1058b167d70
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-04-25 01:09:25 +0530

    Routing Done

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
    }
   return (
     <div className='py-4 w-full flex justify-between px-14 items-center bg-black text-white border-b-2 border-b-amber-50'>
-      <Link className='h-8 flex flex-col justify-center items-center'>
+      <Link to="/" className='h-8 flex flex-col justify-center items-center'>
         <div className='h-full flex'>
           <img src={logo} alt="" className="h-full invert" />
           <h1 className='font-bold pt-2 pl-1 text-red-600 text-xl'>PRO</h1>
@@ -19,17 +19,17 @@ function Navbar() {
         <h1 className='text-[10px] font-serif tracking-widest font-light'>ElectroPro</h1>
       </Link>
       <div className='flex gap-4'>
-        <Link to="/home" className='font-bold text-lg group hover:text-red-600 transition-all duration-200 hover:scale-[1.02]'>Home
-        <div className='w-full h-1 rounded-2xl transition-transform duration-200 ease-in hidden group-hover:flex origin-left bg-white'></div>
+        <Link to="/home" className='NavStyle group'>Home
+        <div className='underLine'></div>
         </Link>
-        <Link to="/product" className='font-bold text-lg group hover:text-red-600 hover:scale-[1.02]'>Product
-        <div className='w-full h-1 rounded-2xl transition-all duration-200 ease-in hidden group-hover:flex bg-white'></div>
+        <Link to="/product" className='NavStyle group'>Product
+        <div className='underLine'></div>
         </Link>
-        <Link to="/cart" className='font-bold text-lg group hover:text-red-600 hover:scale-[1.02]'>Cart
-        <div className='w-full h-1 rounded-2xl transition-all duration-200 ease-in hidden group-hover:flex bg-white'></div>
+        <Link to="/cart" className='NavStyle group'>Cart
+        <div className='underLine'></div>
         </Link>
-        <Link to="/contact" className='font-bold text-lg group hover:text-red-600 hover:scale-[1.02]'>Contact Us
-        <div className='w-full h-1 rounded-2xl transition-all duration-200 ease-in hidden group-hover:flex bg-white'></div>
+        <Link to="/contact" className='NavStyle group'>Contact Us
+        <div className='underLine'></div>
         </Link>
         
       </div>
